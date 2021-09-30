eebf640fa56d409bf4d3f1d77eea87e3fbf7d392
gtasa3box <gtasa3box@gmail.com>
2021-10-01 02:48:31 +0300

    first

--- src/js/global.js
+++ src/js/global.js
@@ -13,3 +13,34 @@ function cislo(){
     if (event.keyCode != 43 && event.keyCode < 48 || event.keyCode > 57)
     event.preventDefault();
 }
+
+//slider
+$('.slider-howto').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+});
