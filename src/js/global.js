38fcb9d5a8fa92030a5dbb43b961c2059f5af190
gtasa3box <gtasa3box@gmail.com>
2021-09-30 19:52:46 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -8,3 +8,8 @@ $('.filters__title').on('click', function(){
     $(this).toggleClass('active') 
     $($(this).data('target')).stop(true, false).slideToggle()  
 });
+//input numbers
+function cislo(){
+    if (event.keyCode != 43 && event.keyCode < 48 || event.keyCode > 57)
+    event.preventDefault();
+}
