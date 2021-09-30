185549d68539614077573acbca594286b33e599e
gtasa3box <gtasa3box@gmail.com>
2021-09-30 17:33:42 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -1,1 +1,10 @@
-console.log('global');
+//burger
+$('#burger').on('click', function(){
+    $('#burgerlink').toggleClass('active')
+    $('.nav').toggleClass('active')
+});
+//dropdown
+$('.filters__title').on('click', function(){
+    $(this).toggleClass('active') 
+    $($(this).data('target')).stop(true, false).slideToggle()  
+});
