2930becc62eae70b98e7631c293fe5681c6f04bc
gtasa3box <gtasa3box@gmail.com>
2021-11-22 18:40:16 +0300

    first commit

--- src/js/global.js
+++ src/js/global.js
@@ -77,6 +77,20 @@ $(function() {
   $('.lazy').Lazy();
 });
 
-//php mailer
-
-// Отправка данных на сервер
+//up-btn
+$(document).ready(function() { 
+  var button = $('.top-btn');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+});	 
+button.on('click', function(){
+$('body, html').animate({
+scrollTop: 0
+}, 800);
+return false;
+});		 
+});
